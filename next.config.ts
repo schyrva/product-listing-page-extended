d7ddb0b8a0b6eadedad624bc2730252aee33ea4f
Stanislav Chyrva <alkefirko@gmail.com>
2025-02-15 17:43:21 +0200

    next api img allow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["api.escuelajs.co"],
+  },
 };
 
 export default nextConfig;
