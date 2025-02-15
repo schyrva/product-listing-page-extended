98b601472f786a9e58770ffce305adbac0a4cf8a
Stanislav Chyrva <alkefirko@gmail.com>
2025-02-15 17:58:50 +0200

    add footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-100 py-6">
+      <div className="container mx-auto px-4">
+        <p className="text-center text-gray-600">© 2023 Store. All rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
